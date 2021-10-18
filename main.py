1538faa18238dc3a3d0f29dd607a18976eaede74
Cobra05 <raistlinmajere10@gmail.com>
2021-10-18 09:21:38 +0500

    Commit

--- main.py
+++ main.py
@@ -1,36 +1,65 @@
 import telebot
-from telebot import types
+from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = telebot.TeleBot('1994419215:AAGDQiKTaYk2biFT_5_smPMJ_WRbKs82xRw')
 
-startMarkup = types.ReplyKeyboardMarkup()
-startMarkup.row("Ish Beruvchi")
-startMarkup.row("Ishchi")
-
-countMarkup = types.ReplyKeyboardMarkup()
-countMarkup.row("Bir", "Ikki", "Uch")
-countMarkup.row("Ishchi guruh")
+# variables
+lang = 'ru'
 
 
+# markups
+def lang_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton('Russ', callback_data='lang_ru'),
+               InlineKeyboardButton('O\'zbekcha', callback_data='lang_uz'))
+    return markup
+
+
+def type_markup_uz():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton('Ish Beruvchi', callback_data='type_emple'),
+               InlineKeyboardButton('Ishchi', callback_data='type_emplr'))
+    return markup
+def type_markup_ru():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton('Работодатель', callback_data='type_emple'),
+               InlineKeyboardButton('Работник', callback_data='type_emplr'))
+    return markup
+
+
+# bot
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.from_user.id, "Assalomu aleykum\n"
-                                           "Testbot ga xush kellibsiz\n"
-                                           "Tanlang:\n", reply_markup=startMarkup)
-    bot.register_next_step_handler(message, callback=employer)
+    bot.send_message(message.chat.id, 'Assalomu aleykum\nFoydalanish tilini tanlang:\n'
+                                      '\nRus...:', reply_markup=lang_markup())
+
+
 
 
 @bot.message_handler(content_types=['text'])
-def employer(message):
-    if message.text == "Ish Beruvchi":
-        bot.send_message(message.from_user.id, "Ishchilar sonini tanlang:")
-        bot.register_next_step_handler(message, employerCountStep)
+def start_type(message):
+    if lang == 'uz':
+        bot.send_message(message.chat.id, 'Ish bervchimisiz yoki ishchi?', reply_markup=type_markup_uz())
+    elif lang == 'ru':
+        bot.send_message(message.chat.id, 'Вы работодатель или работник?', reply_markup=type_markup_ru())
 
-def employerCountStep(message):
-    if message.text == "Bir" or "Ikki" or "Uch":
-        bot.send_message(message.from_user.id, "Qaysi soxa mutaxassislarini izlamoqdasiz?")
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    global lang
+    if call.data == 'lang_ru':
+        bot.answer_callback_query(call.id, text='Selected Russian')
+        lang = "ru"
+        bot.register_next_step_handler(call.message, callback=start_type)
+    elif call.data == "lang_uz":
+        bot.answer_callback_query(call.id, 'Selected Uzbekisch')
+        lang = 'uz'
+        bot.register_next_step_handler(call.message, callback=start_type)
 
 
 
 
-bot.polling()
+
+bot.infinity_polling()
